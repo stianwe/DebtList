c9890cd7658c9260f12082d79a7541f1b2e9ed91
Runar B. Olsen <runar.b.olsen@gmail.com>
2012-11-08 16:51:01 +0100

    Restore from XML works for basic types Missing object placeholders and a referance rebuild phase.

--- xml/XMLSerializable.java
+++ xml/XMLSerializable.java
@@ -1,11 +1,21 @@
 package xml;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 /**
  * A object that supports serializing to and from a simple XML format
@@ -78,7 +88,7 @@ abstract public class XMLSerializable {
 	 * @return
 	 */
 	public String toXML() {
-		return toXML(null);
+		return "<xml>"+toXML(null)+"</xml>";
 	}
 	
 	/**
@@ -137,26 +147,138 @@ abstract public class XMLSerializable {
 		}
 	}
 	
-	public static void main(String[] args) {
-		XMLSerializable g = new XMLSerializable(){
-			protected String getId() {
-				return "group-1";
-			}
-		};
-		XMLSerializable o = new XMLSerializable() {			
-			@Override
-			protected String getId() {
-				return "1";
-			}
-		};
-		List<XMLSerializable> list = new LinkedList<XMLSerializable>();
-		list.add(o);
-		g.setVariable("users", list);
-		o.setVariable("id", 1);
-		o.setVariable("username", "runar");
-		o.setVariable("group", g);
+	/**
+	 * This object is set to the result object by the SAX handler after
+	 * parsing the entire xml 
+	 */
+	private static Object toObject;
+	
+	/**
+	 * SAX handler used for object restoration
+	 */
+	private static DefaultHandler handler = new DefaultHandler() {
 		
-		System.out.println(o.toXML());
+		private Map<String, XMLSerializable> reg;
+		private XMLSerializable object;
+		private String varName;
+		private String type;
+		private List list;
+		
+		@Override
+		public void startElement(String uri, String localName, String qName,
+				Attributes attributes) throws SAXException {
+			if(localName.equals("xml") && reg == null) {
+				reg = new HashMap<String, XMLSerializable>();
+				varName = type = null; list = null; object = null;
+			} else if(object == null) {
+				try {
+					Object o = getClass().getClassLoader().loadClass(localName)
+							.newInstance();
+					if(o instanceof XMLSerializable) {
+						object = (XMLSerializable) o;
+					} else {
+						throw new RuntimeException("Unknown class name "+localName);
+					}
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if(varName == null) {
+				varName = localName;
+			} else if(type == null) {
+				if(localName.equals("list")) {
+					list = new LinkedList();
+					object.setVariable(varName, list);
+				} else {
+					type = localName;
+				}
+			}				
+		}
+		
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			if(list != null && localName.equals("list")) {
+				list = null;
+				varName = null;
+			} else if(object != null && localName.equals(object.getClass().getName())) {
+				reg.put(object.getClass().getName(), object);
+				toObject = object;
+				object = null;				
+			}
+		};
+		
+		@Override
+		public void characters(char[] ch, int start, int length)
+				throws SAXException {
+			if(varName != null && type != null) {
+				Object obj = toObjectHelper(new String(ch, start, length));
+				if(list != null) {
+					list.add(obj);
+					type = null;
+				} else {
+					object.setVariable(varName, obj);
+					varName = type = null;
+				}
+			}
+		}
+		
+		/**
+		 * Convert a data string to the current type
+		 * 
+		 * @param data
+		 * @return
+		 */
+		private Object toObjectHelper(String data) {
+			if(type.equals("int")) {
+				return Integer.parseInt(data);
+			} else if(type.equals("string")) {
+				return data;
+			} else {
+				System.err.println("Placeholder for object");
+			}
+			return null;
+		}
+		
+	};
+	
+	/**
+	 * Convert the given XML back into object form
+	 * 
+	 * @param xml
+	 */
+	public static Object toObject(String xml) {
+		XMLReader xr;
+		try {
+			xr = XMLReaderFactory.createXMLReader();
+	        xr.setContentHandler(handler);
+	        xr.setErrorHandler(handler);
+	        InputStream xmlStream = new ByteArrayInputStream(
+	        		xml.getBytes("UTF-8"));
+	        xr.parse(new InputSource(xmlStream));
+	        
+	        return toObject;	        
+	        
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		User u = new User();
+		System.out.println(u.toXML());
+		User u1 = (User) XMLSerializable.toObject(u.toXML());
+		System.out.println(u1.getVariable("username"));
+		
 		
 	}
 	
